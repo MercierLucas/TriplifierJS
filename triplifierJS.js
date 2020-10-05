e62a4cd07e1ed1f571ea2c6bf9a735d01d859260
LucasMercier <lucas.mercier@isep.fr>
2020-10-05 17:51:28 +0200

    Fixing end line not working properly

--- triplifierJS.js
+++ triplifierJS.js
@@ -159,7 +159,7 @@ var TriplifierJS = function()
         var predicats = [];
 
         var startLine = startingLine == 0 ? (headerLine == 0 ? 1 : 0) : startingLine;
-        var endLine = endingLine == -1 ? lines.length - 1 : endingLine;
+        var endLine = endingLine == -1 ? lines.length : endingLine;
 
         if(headerLine != 0)
         {
